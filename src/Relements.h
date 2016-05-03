5afa8233ec4266c3541962b75a03e5961ccef0f2
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/Rchemcpp@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/Relements.h
@@ -0,0 +1,17 @@
+
+#ifndef RELEMENTS_H
+#define RELEMENTS_H
+
+#include <Rcpp.h>
+
+#include <elements.h>
+
+extern Elements elements;
+
+//---------
+// external
+//---------
+
+void loadGramAtoms( std::string aFileName );
+
+#endif
